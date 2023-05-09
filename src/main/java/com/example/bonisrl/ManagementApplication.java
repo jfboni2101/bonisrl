9a2e5e9743d9acd2b9cf742d6416a2c2214c491c
jfboni2101 <matte.boni2101@gmail.com>
2023-05-09 08:45:11 +0200

    Person class

--- src/main/java/com/example/bonisrl/ManagementApplication.java
+++ src/main/java/com/example/bonisrl/ManagementApplication.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ManagementApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-page.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ManagementApplication.class.getResource("login-page.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Boni S.R.L.");
         stage.setScene(scene);
---
+++ src/main/java/com/example/bonisrl/Person.java
@@ -0,0 +1,7 @@
+package com.example.bonisrl;
+
+public class Person {
+    String firstName;
+    String lastName;
+    String birthday;
+}
